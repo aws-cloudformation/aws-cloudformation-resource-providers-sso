d252ea93572c1cf5c3e1737c7ce3f3cb03853eb1
Andrii Glukhyi <glukhyi@amazon.com>
2021-01-12 15:59:39 -0800

    Making read handler return response in sync

--- aws-sso-permissionset/src/main/java/software/amazon/sso/permissionset/ReadHandler.java
+++ aws-sso-permissionset/src/main/java/software/amazon/sso/permissionset/ReadHandler.java
@@ -1,9 +1,8 @@
 package software.amazon.sso.permissionset;
 
 import software.amazon.awssdk.services.ssoadmin.SsoAdminClient;
-import software.amazon.awssdk.services.ssoadmin.model.DescribePermissionSetResponse;
-import software.amazon.awssdk.services.ssoadmin.model.ResourceNotFoundException;
-import software.amazon.awssdk.services.ssoadmin.model.ThrottlingException;
+import software.amazon.awssdk.services.ssoadmin.model.*;
+import software.amazon.cloudformation.exceptions.CfnThrottlingException;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
 import software.amazon.cloudformation.proxy.HandlerErrorCode;
 import software.amazon.cloudformation.proxy.Logger;
@@ -12,6 +11,8 @@ import software.amazon.cloudformation.proxy.ProxyClient;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 import software.amazon.sso.permissionset.actionProxy.InlinePolicyProxy;
 import software.amazon.sso.permissionset.actionProxy.ManagedPolicyAttachmentProxy;
+
+import java.security.SecureRandom;
 import java.util.List;
 
 import static software.amazon.sso.permissionset.utils.Constants.RETRY_ATTEMPTS;
@@ -19,15 +20,17 @@ import static software.amazon.sso.permissionset.utils.Constants.RETRY_ATTEMPTS_Z
 import static software.amazon.sso.permissionset.utils.TagsUtil.getResourceTags;
 
 public class ReadHandler extends BaseHandlerStd {
+    private static final String THROTTLE_MESSAGE = "Read request got throttled. Please add DependsOn attribute if you have large number of AWS SSO owned resources";
+    private static final SecureRandom SECURE_RANDOM = new SecureRandom();
     private Logger logger;
     private List<Tag> tags;
 
     protected ProgressEvent<ResourceModel, CallbackContext> handleRequest(
-        final AmazonWebServicesClientProxy proxy,
-        final ResourceHandlerRequest<ResourceModel> request,
-        final CallbackContext callbackContext,
-        final ProxyClient<SsoAdminClient> proxyClient,
-        final Logger logger) {
+            final AmazonWebServicesClientProxy proxy,
+            final ResourceHandlerRequest<ResourceModel> request,
+            final CallbackContext callbackContext,
+            final ProxyClient<SsoAdminClient> proxyClient,
+            final Logger logger) {
 
         this.logger = logger;
 
@@ -40,54 +43,86 @@ public class ReadHandler extends BaseHandlerStd {
 
         return ProgressEvent.progress(model, callbackContext)
                 .then(progress -> proxy.initiate("AWS-SSO-PermissionSet::Read", proxyClient, model, callbackContext)
-                    .translateToServiceRequest(Translator::translateToReadRequest)
-                    .makeServiceCall((readRequest, client) -> {
-                        DescribePermissionSetResponse response = proxy.injectCredentialsAndInvokeV2(readRequest, client.client()::describePermissionSet);
-                        if (tags == null || tags.isEmpty()) {
-                            tags = Translator.ConvertToModelTag(getResourceTags(readRequest.instanceArn(),
-                                    model.getPermissionSetArn(),
-                                    proxy,
-                                    proxyClient));
-                        }
+                        .translateToServiceRequest(Translator::translateToReadRequest)
+                        .makeServiceCall((readRequest, client) -> {
+                            DescribePermissionSetResponse response = null;
+                            int psRetryAttempts = Integer.valueOf(RETRY_ATTEMPTS);
+                            while (psRetryAttempts > RETRY_ATTEMPTS_ZERO) {
+                                try {
+                                    response = proxy.injectCredentialsAndInvokeV2(readRequest, client.client()::describePermissionSet);
+                                    if (tags == null || tags.isEmpty()) {
+                                        tags = Translator.ConvertToModelTag(getResourceTags(readRequest.instanceArn(),
+                                                model.getPermissionSetArn(),
+                                                proxy,
+                                                proxyClient));
+                                    }
 
-                        logger.log(String.format("%s has successfully been read.", ResourceModel.TYPE_NAME));
-                        return response;
-                    })
-                    .handleError((describePermissionSetRequest, exception, client, resourceModel, context) -> {
-                        if (exception instanceof ResourceNotFoundException) {
-                            return ProgressEvent.defaultFailureHandler(exception, HandlerErrorCode.NotFound);
-                        } else if (exception instanceof ThrottlingException && context.getRetryAttempts() >= RETRY_ATTEMPTS_ZERO) {
-                            context.decrementRetryAttempts();
-                            return ProgressEvent.defaultInProgressHandler(context, 5, model);
-                        }
-                        throw exception;
-
-                    })
-                    .done((readRequest, readResponse, proxyInvocation, resourceModel, context) -> {
-                        ResourceModel outputModel = Translator.translateFromReadResponse(readResponse, resourceModel.getInstanceArn(), tags);
-
-                        context.resetRetryAttempts(RETRY_ATTEMPTS);
-                        return ProgressEvent.defaultInProgressHandler(context, 0, outputModel);
-                    })
+                                    logger.log(String.format("%s has successfully been read.", ResourceModel.TYPE_NAME));
+                                    break;
+                                } catch (ThrottlingException | InternalServerException e) {
+                                    psRetryAttempts = decrementAndWait(psRetryAttempts);
+                                    continue;
+                                }
+                            }
+                            if(response != null) {
+                                return response;
+                            } else {
+                                throw new CfnThrottlingException(THROTTLE_MESSAGE);
+                            }
+                        })
+                        .handleError((describePermissionSetRequest, exception, client, resourceModel, context) -> {
+                            if (exception instanceof ResourceNotFoundException) {
+                                return ProgressEvent.defaultFailureHandler(exception, HandlerErrorCode.NotFound);
+                            } else if(exception instanceof AccessDeniedException){
+                                return ProgressEvent.defaultFailureHandler(exception, HandlerErrorCode.AccessDenied);
+                            } else if (exception instanceof ValidationException) {
+                                return ProgressEvent.defaultFailureHandler(exception, HandlerErrorCode.InvalidRequest);
+                            } else {
+                                return ProgressEvent.defaultFailureHandler(exception, HandlerErrorCode.InternalFailure);
+                            }
+                        })
+                        .done((readRequest, readResponse, proxyInvocation, resourceModel, context) -> {
+                            ResourceModel outputModel = Translator.translateFromReadResponse(readResponse, resourceModel.getInstanceArn(), tags);
+                            context.resetRetryAttempts(RETRY_ATTEMPTS);
+                            return ProgressEvent.defaultInProgressHandler(context, 0, outputModel);
+                        })
                 )
                 .then(progress -> {
                     ResourceModel outputModel = progress.getResourceModel();
                     CallbackContext context = progress.getCallbackContext();
-
-                    try {
-                        outputModel.setManagedPolicies(managedPolicyAttachmentProxy.getAttachedManagedPolicies(outputModel.getInstanceArn(),
-                                outputModel.getPermissionSetArn()));
-                        outputModel.setInlinePolicy(inlinePolicyProxy.getInlinePolicyForPermissionSet(outputModel.getInstanceArn(),
-                                outputModel.getPermissionSetArn()));
-                    } catch (ThrottlingException e) {
-                        if (context.getRetryAttempts() == RETRY_ATTEMPTS_ZERO) {
-                            throw e;
+                    int mpRetryAttempts = Integer.valueOf(RETRY_ATTEMPTS);
+                    while (mpRetryAttempts > RETRY_ATTEMPTS_ZERO) {
+                        try {
+                            outputModel.setManagedPolicies(managedPolicyAttachmentProxy.getAttachedManagedPolicies(outputModel.getInstanceArn(),
+                                    outputModel.getPermissionSetArn()));
+                            outputModel.setInlinePolicy(inlinePolicyProxy.getInlinePolicyForPermissionSet(outputModel.getInstanceArn(),
+                                    outputModel.getPermissionSetArn()));
+                            break;
+                        } catch (ThrottlingException | InternalServerException e) {
+                            mpRetryAttempts = decrementAndWait(mpRetryAttempts);
+                            continue;
                         }
-                        context.decrementRetryAttempts();
-
-                        return ProgressEvent.defaultInProgressHandler(progress.getCallbackContext(), 5, outputModel);
                     }
-                    return ProgressEvent.defaultSuccessHandler(outputModel);
+                    if(mpRetryAttempts > RETRY_ATTEMPTS_ZERO) {
+                        return ProgressEvent.defaultSuccessHandler(outputModel);
+                    } else {
+                        return ProgressEvent.defaultFailureHandler(ThrottlingException.builder().message(THROTTLE_MESSAGE).build(),
+                                HandlerErrorCode.Throttling);
+                    }
                 });
+    }
+
+    /**
+     * Decrement context and wait for some time
+     * Should wait between 1 and 5 second in case got throttled.
+     */
+    private int decrementAndWait(int attempts) {
+        int timeToWait = SECURE_RANDOM.ints(1000, 5000).findFirst().getAsInt();
+        try {
+            Thread.sleep(timeToWait);
+        } catch (InterruptedException e) {
+            // Ignore if fail sleep
+        }
+        return attempts - 1;
     }
 }
